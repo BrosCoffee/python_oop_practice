1a661cecf46c363e6938eb8e091ed0dc2fcd0e22
Raymond <raymondcyang0219@gmail.com>
2019-08-13 21:47:18 +0800

    card.py 2nd edited

--- card.py
+++ card.py
@@ -13,35 +13,18 @@ class Card:
     def __str__(self):
         return "{} of {}".format(self.val, self.suit)
 
-    # def __lt__(self, other):
-    #
-    # # return comparison
-    # def __le__(self, other):
-    #
-    # # return comparison
-    # def __eq__(self, other):
-    #
-    # # return comparison
-    # def __ne__(self, other):
-    #
-    # # return comparison
-    # def __gt__(self, other):
-    #
-    # # return comparison
-    # def __ge__(self, other):
-    #
-    # # return comparison
-
+    @property
     def value(self):
-        return val.values('{}'.format(self.val))
-
-
+        return val[self.val]
 
 class Deck:
 
-    def __init__(self):
+    def __init__(self, deck_num):
+        self.deck_num = deck_num
         self.cards = []
-        self.build()
+        while self.deck_num > 0:
+            self.build()
+            self.deck_num -= 1
 
     def __str__(self):
         return "It is a deck of cards."
@@ -51,26 +34,28 @@ class Deck:
             for n in val:
                 self.cards.append(Card(s, n))
 
-
-
 class Player:
 
     def __init__(self):
         pass
 
-# card_1 = Card('Spades', 'Ace')
-# card_1.show()
+deck_1 = Deck(3) # Using 3 decks of cards
+# for i in deck_1.cards:
+#     print(i)
+# print(len(deck_1.cards)) # How many cards in the big
 
-deck_1 = Deck()
 card_1 = random.choice(deck_1.cards)
 card_2 = random.choice(deck_1.cards)
 card_3 = random.choice(deck_1.cards)
 
-print(card_1)
-print(card_2)
-# print(card_1.value > card_2.value)
-
+print("Card_1:", card_1)
+print("Card_2:", card_2)
+print("Card_3:", card_3)
+print("Card_1's value:", card_1.value)
+print("Is card_2 > card_3:", card_2.value > card_3.value)
+print("card_1 + card_2 + card_3:", card_1.value + card_2.value + card_3.value)
 '''
-when print() it will print 'X of X'
-when compare two cards: card_1 == card_2: False, card_1 > card_2: True
+1. when print() it will print 'X of X --> line 13, 14
+2. when compare two cards: card_1.value == card_2.value >>> False,
+   card_1.value > card_2.value >>> True --> line 16- 18
 '''
